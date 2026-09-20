8de1312eb2af6b51e2057725404461b1062e43c0
Ruan Victor <ruanvclima@gmail.com>
2026-09-20 04:56:29 -0300

    feat: created the task-manager project

---
+++ src/routes/index.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+import { userRoutes } from "./users-routes";
+
+const routes = Router()
+
+
+//public routes
+routes.use(userRoutes)
+
+export {routes}
---
+++ src/server.ts
@@ -0,0 +1,5 @@
+import { app } from "./app";
+
+const PORT = 3333
+
+app.listen(PORT, () => console.log(`Server os is running on port ${PORT}`))
