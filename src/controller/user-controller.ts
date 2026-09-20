35351f91d05d2c1f90778b0a35436dc3b1a88ed2
Ruan Victor <ruanvclima@gmail.com>
2026-09-20 05:10:25 -0300

    feat: created error handling

--- src/controller/user-controller.ts
+++ src/controller/user-controller.ts
@@ -1,7 +1,10 @@
 import {Request, Response} from "express"
+import { AppError } from "../utils/AppError"
 
 class UserController{
     async create(request: Request, response: Response){
+
+        throw new AppError("Teste", 400)
         return response.json({message: "ok"})
     }
 }
