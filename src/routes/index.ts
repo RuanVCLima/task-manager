446daef5edcf22a6105afc1ae414c3f981ba62ad
Ruan Victor <ruanvclima@gmail.com>
2026-09-20 13:05:24 -0300

    feat: verified user authorization and verified user token

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,17 @@ import { Router } from 'express';
 
 import { userRoutes } from './users-routes';
 import { sessionsRoutes } from './sessions-routes';
+import { ensureAthenticated } from '../middleware/ensure-authenticated';
+import { teamsRoutes } from './teams-routes';
 
 const routes = Router();
 
 //public routes
 routes.use('/users', userRoutes);
 routes.use('/sessions', sessionsRoutes);
+
+//private routes
+routes.use(ensureAthenticated);
+routes.use('/teams', teamsRoutes);
 
 export { routes };
---
+++ src/routes/teams-routes.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import { TeamsController } from '../controller/teams-controller';
+import { verifyUserAuthorization } from '../middleware/verify-user-authorization';
+
+const teamsRoutes = Router();
+const teamsController = new TeamsController();
+
+teamsRoutes.post(
+  '/',
+  verifyUserAuthorization(['admin']),
+  teamsController.create,
+);
+
+export { teamsRoutes };
