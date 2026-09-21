fa5b0b08568d00d03766fb5d587415cfc6bf712c
Ruan Victor <ruanvclima@gmail.com>
2026-09-21 08:04:49 -0300

    feat: created the create function in tasks

--- src/controller/task-controller.ts
+++ src/controller/task-controller.ts
@@ -1,8 +1,36 @@
 import { Request, Response } from 'express';
+import { z } from 'zod';
+import { prisma } from '../database/prisma';
 
 class TaskController {
   async create(request: Request, response: Response) {
-    return response.json({ message: 'ok' });
+    const StatusEnum = z.enum(['pending', 'inProgress', 'completed']);
+
+    const PriorityEnum = z.enum(['high', 'medium', 'low']);
+    const bodySchema = z.object({
+      title: z.string().min(1, 'title is required'),
+      description: z.string().min(1, 'description is required'),
+      status: StatusEnum,
+      priority: PriorityEnum,
+      assignedTo: z.uuid(),
+      teamId: z.uuid(),
+    });
+
+    const { title, description, status, priority, assignedTo, teamId } =
+      bodySchema.parse(request.body);
+
+    const task = await prisma.tasks.create({
+      data: {
+        title,
+        description,
+        status,
+        priority,
+        assignedTo,
+        teamId,
+      },
+    });
+
+    return response.json(task);
   }
 }
 
