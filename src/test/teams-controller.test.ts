53207e62feb7ccb4761e06105e7da7a1e5215485
Ruan Victor <ruanvclima@gmail.com>
2026-09-22 11:28:14 -0300

    fix: adjusted the order in test, and created a beforeAll to create a test user an test session

--- src/test/teams-controller.test.ts
+++ src/test/teams-controller.test.ts
@@ -6,13 +6,14 @@ import { app } from '../app';
 describe('TeamsController', () => {
   let user_id: string;
   let teams_id: string;
+  let token: string;
 
   afterAll(async () => {
     await prisma.teams.delete({ where: { id: teams_id } });
     await prisma.users.delete({ where: { id: user_id } });
   });
 
-  it('should create a new team successfully', async () => {
+  beforeAll(async () => {
     const response = await request(app).post('/users').send({
       name: 'Team Test User',
       email: 'teamtestuser@example.com',
@@ -27,21 +28,25 @@ describe('TeamsController', () => {
       password: 'password123',
     });
 
-    const { token } = sessionsResponse.body;
+    token = sessionsResponse.body.token;
+  });
 
-    const teamsResponse = await request(app)
-      .post('/teams')
-      .set('Authorization', `Bearer ${token}`)
-      .send({
-        name: 'team test',
-        description: 'description test',
-        userId: user_id,
-      });
+  describe('POST/teams', () => {
+    it('should create a new team successfully', async () => {
+      const teamsResponse = await request(app)
+        .post('/teams')
+        .set('Authorization', `Bearer ${token}`)
+        .send({
+          name: 'team test',
+          description: 'description test',
+          userId: user_id,
+        });
 
-    teams_id = teamsResponse.body.id;
+      teams_id = teamsResponse.body.id;
 
-    expect(teamsResponse.status).toBe(201);
-    expect(teamsResponse.body).toHaveProperty('id');
-    expect(teamsResponse.body.name).toBe('team test');
+      expect(teamsResponse.status).toBe(201);
+      expect(teamsResponse.body).toHaveProperty('id');
+      expect(teamsResponse.body.name).toBe('team test');
+    });
   });
 });
